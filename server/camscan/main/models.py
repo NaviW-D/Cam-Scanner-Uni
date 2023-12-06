bb7fa46fb8509c91b4a1fc62a0e13be5c86d858c
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-06 14:06:08 +0330

    make CarTag models in django app

--- server/camscan/main/models.py
+++ server/camscan/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class CarTag(models.Model):
+    tag = models.TextField(max_length=8)
+    availability = models.BooleanField()
+
+    def __str__(self):
+        return self.tag
