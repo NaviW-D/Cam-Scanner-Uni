c5d8b4427ecaac37dc45a5156be67648a40ce292
sadranamavar <amirsadra.namavar@gmail.com>
2023-12-06 14:19:18 +0330

    add data time in car tag model in django app

--- server/camscan/main/models.py
+++ server/camscan/main/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class CarTag(models.Model):
     tag = models.TextField(max_length=8)
     availability = models.BooleanField()
+    #times data
+    register_time = models.DateTimeField( auto_now_add=True)
+    expiration_time = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.tag
